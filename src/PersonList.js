20d74a66ab0e02ba0d8789358d2c0a826a64a42c
Emad <emadk571@gmail.com>
2022-11-20 02:46:47 -0500

    added personlist and  app.js

---
+++ src/PersonList.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import axios from "axios";
+export default class PersonList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      persons: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`https://randomuser.me/api/?results=10`).then((res) => {
+      const persons = res.data.results;
+      this.setState({ persons: persons });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.persons.map((person) => (
+          <>
+            <div>
+            <p style={{ backgroundColor: "#00CCCC"}}>
+                {person.name.title} {person.name.first} {person.name.last}
+                {person.login.uuid}
+              </p>
+              <div style={{ backgroundColor: "#00FFFF"}}>
+                <div>
+                  <img
+                    src={person.picture.large}
+                    alt="persons"
+                    style={{
+                      float: "left",
+                    }}
+                  />
+                  <button
+                    onClick={this.componentDidMount}
+                    style={{
+                      textAlign: "center",
+                      position: "relative",
+                      border_radius: "20px",
+                      color: "Black"
+                    }}
+                  >
+                    {" "}
+                    Details{" "}
+                  </button>
+                </div>
+               
+                <div style={{ display: "inline-block" }}>
+                  <p>User Name: {person.login.username}</p>
+                  <p>Gender: {person.gender}</p>
+                  <p> Time Zone Description :{" "}{person.location.timezone.description}</p>
+                  <p>Address: {person.location.street.number}{" "}
+                    {person.location.street.name}, {person.location.city},{" "}
+                    {person.location.state}, {person.location.country},{" "}
+                    {person.location.postcode}
+                  </p>
+                  <p>Email: {person.email}</p>
+                  <p>Birth Date and Age: {person.dob.date} - ({person.dob.age})</p>
+                  <p>Register Date: {person.registered.date}</p>
+                  <p>Phone#: {person.phone}</p>
+                  <p>Cell#: {person.cell}</p>
+                </div>
+              </div>
+              <hr></hr>
+              <hr></hr>
+            </div>
+          </>
+        ))}
+      </div>
+    );
+  }
+}
